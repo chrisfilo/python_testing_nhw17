bd77a5e99e7ea5962c903b946784bafc9bb1dbd9
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2018-07-30 16:43:42 -0700

    break

--- demean_age.py
+++ demean_age.py
@@ -3,7 +3,7 @@ import numpy as np
 
 age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
 
-mean_age = sum(age)/len(age)
+mean_age = sum(age)/len(agehdsffjkahgfkjdah)
 
 np.savetxt("demeaned_" + sys.argv[1], age-mean_age)
 
